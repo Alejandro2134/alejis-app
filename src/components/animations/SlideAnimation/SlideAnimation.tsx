5107c29e7d8d05305dbf73973f700a54e4ac6004
Alejandro Zapata <zapatamolinaalejandro@gmail.com>
2024-06-02 16:44:23 -0500

    feat: slideAnimation component

--- src/components/animations/SlideAnimation/SlideAnimation.tsx
+++ src/components/animations/SlideAnimation/SlideAnimation.tsx
@@ -1,45 +1,113 @@
-import React, {ReactNode, useEffect, useRef} from 'react';
-import {Animated, View} from 'react-native';
+import React, {ReactNode, useEffect, useRef, useState} from 'react';
+import {Animated, StyleSheet, View, LayoutChangeEvent} from 'react-native';
 
 type Props = {
   previousMonth: ReactNode;
-  nextMonth?: ReactNode;
+  nextMonth: ReactNode;
   currentMonth: ReactNode;
+  activateAnimationToTheRight: boolean;
+  activateAnimationToTheLeft: boolean;
+  animationDuration: number;
+  deactivateAnimation: () => void;
+};
+
+type Layout = {
+  width: number;
+  height: number;
 };
 
 const SlideAnimation: React.FC<Props> = ({
   currentMonth,
   previousMonth,
+  nextMonth,
+  activateAnimationToTheRight,
+  activateAnimationToTheLeft,
+  animationDuration,
+  deactivateAnimation,
 }: Props) => {
-  const xPosition = useRef(new Animated.Value(0)).current;
+  const previousMonthXPosition = useRef(new Animated.Value(0)).current;
+  const currentMonthXPosition = useRef(new Animated.Value(0)).current;
+  const nextMonthXPosition = useRef(new Animated.Value(0)).current;
+  const [layout, setLayout] = useState<Layout>({width: 0, height: 0});
+
+  const startAnimation = (isForNextMonth: boolean) => {
+    const translateMonth = isForNextMonth
+      ? nextMonthXPosition
+      : previousMonthXPosition;
+    const translateMonthToValue = isForNextMonth ? -layout.width : layout.width;
+
+    Animated.parallel([
+      Animated.timing(translateMonth, {
+        toValue: translateMonthToValue,
+        duration: animationDuration,
+        useNativeDriver: true,
+      }),
+      Animated.timing(currentMonthXPosition, {
+        toValue: translateMonthToValue,
+        duration: animationDuration,
+        useNativeDriver: true,
+      }),
+    ]).start(() => {
+      deactivateAnimation();
+      previousMonthXPosition.setValue(0);
+      currentMonthXPosition.setValue(0);
+      nextMonthXPosition.setValue(0);
+    });
+  };
 
   useEffect(() => {
-    Animated.timing(xPosition, {
-      toValue: 400,
-      duration: 1000,
-      useNativeDriver: true,
-    }).start();
+    if (activateAnimationToTheRight) {
+      startAnimation(true);
+    }
+
+    if (activateAnimationToTheLeft) {
+      startAnimation(false);
+    }
   });
 
-  return (
-    <View>
-      <Animated.View
-        style={{
-          transform: [{translateX: xPosition}],
-          elevation: 100,
-          position: 'absolute',
-          width: '100%',
-          left: -400,
-        }}>
-        {previousMonth}
-      </Animated.View>
+  const onLayoutHandler = (e: LayoutChangeEvent) => {
+    const {height, width} = e.nativeEvent.layout;
+    setLayout({height, width});
+  };
 
-      <Animated.View
-        style={{elevation: 0, position: 'absolute', width: '100%'}}>
-        {currentMonth}
-      </Animated.View>
+  return (
+    <View onLayout={onLayoutHandler}>
+      {layout && (
+        <>
+          <Animated.View
+            style={{
+              transform: [{translateX: previousMonthXPosition}],
+              right: layout.width,
+              ...styles.months,
+            }}>
+            {previousMonth}
+          </Animated.View>
+          <Animated.View
+            style={{
+              transform: [{translateX: currentMonthXPosition}],
+              ...styles.months,
+            }}>
+            {currentMonth}
+          </Animated.View>
+          <Animated.View
+            style={{
+              transform: [{translateX: nextMonthXPosition}],
+              left: layout.width,
+              ...styles.months,
+            }}>
+            {nextMonth}
+          </Animated.View>
+        </>
+      )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  months: {
+    position: 'absolute',
+    width: '100%',
+  },
+});
 
 export default SlideAnimation;
